8ac2fedd7480c9d3d7b97b39ef25ce8dfb70bb11
Vivek VG <vivekvggit@gmail.com>
2023-09-21 21:13:15 +0530

    Refactored changenotifierproxyprovider to proxyprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:weather_app/models/weather.dart';
 import 'package:weather_app/pages/home_page.dart';
 import 'package:weather_app/providers/providers.dart';
 import 'package:weather_app/providers/theme/theme_provider.dart';
-import 'package:weather_app/providers/weather/weather_provider.dart';
 import 'package:weather_app/repositories/weather_repository.dart';
 import 'package:weather_app/services/weather_api_services.dart';
 import 'package:http/http.dart' as http;
@@ -36,28 +36,25 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (context) => TempSettingsProvider(),
         ),
-        ChangeNotifierProxyProvider<WeatherProvider, ThemeProvider>(
-          create: (context) => ThemeProvider(),
+        ProxyProvider<WeatherProvider, ThemeProvider>(
           update: (
             BuildContext context,
             WeatherProvider weatherProvider,
-            ThemeProvider? themeProvider,
+            _,
           ) =>
-              themeProvider!..update(weatherProvider),
+              ThemeProvider(wp: weatherProvider),
         )
       ],
-      child: Builder(
-        builder: (context) {
-          return MaterialApp(
-            title: 'Flutter Demo',
-            theme: context.watch<ThemeProvider>().state.appTheme == AppTheme.light
-                ? ThemeData.light()
-                : ThemeData.dark(),
-            debugShowCheckedModeBanner: false,
-            home: const HomePage(),
-          );
-        }
-      ),
+      child: Builder(builder: (context) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: context.watch<ThemeProvider>().state.appTheme == AppTheme.light
+              ? ThemeData.light()
+              : ThemeData.dark(),
+          debugShowCheckedModeBanner: false,
+          home: const HomePage(),
+        );
+      }),
     );
   }
 }
--- lib/providers/theme/theme_provider.dart
+++ lib/providers/theme/theme_provider.dart
@@ -1,19 +1,22 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+
 import 'package:weather_app/constants/constants.dart';
 import 'package:weather_app/providers/providers.dart';
+
 part 'theme_state.dart';
 
-class ThemeProvider with ChangeNotifier {
-  ThemeState _state = ThemeState.inital();
-  ThemeState get state => _state;
+class ThemeProvider {
+  final WeatherProvider wp;
+  ThemeProvider({
+    required this.wp,
+  });
 
-  void update(WeatherProvider wp) {
+  ThemeState get state {
     if (wp.state.weather.temp > kWarmorNot) {
-      _state = _state.copyWith(appTheme: AppTheme.light);
+      return ThemeState();
     } else {
-      _state = _state.copyWith(appTheme: AppTheme.dark);
+      return ThemeState(appTheme: AppTheme.dark);
     }
-    notifyListeners();
   }
 }
